be9ccdbbd0b5beb798210a72e4551799f2d78838
Erwan Lievre <lievre.erwan@gmail.com>
2026-09-08 14:40:17 +0200

    api sélection room test

---
+++ escape_engine_api/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Bienvenue sur l'API pour mon super escape game!"}
+
+rooms = {
+    "room1": {"name": "Room 1", "description": "This is room 1"},
+    "room2": {"name": "Room 2", "description": "This is room 2"}
+}
+
+@app.get("/room")
+def get_rooms():
+    return list(rooms.keys())
+
+@app.get("/room/{room_id}")
+def get_room(room_id: str):
+    return rooms.get(room_id)
