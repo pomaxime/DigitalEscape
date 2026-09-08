5d4656db112921375a783f36b8e66408d0d9cb2f
pomaxime <maximepoyet330@gmail.com>
2026-09-08 17:53:34 +0200

    MAJ

--- escape_engine_api/app/routers/player_game.py
+++ escape_engine_api/app/routers/player_game.py
@@ -46,24 +46,18 @@ room_1 = Room(
     description=(
         "Vous êtes enfermés dans un laboratoire rempli de machines étranges "
         "et de dinosaures enfermés. "
-
         "Une inscription affiche RAWR. "
-
         "Un Raptor s'approche de vous et vous regarde avec insistance. "
-
         "Vous entrez dans une pièce avec un ordinateur et un cadenas. "
 
         "Pour avancer vous devrez trouver la signification de 'RAWR'. "
-
         "Pour cela, aidez-vous des instructions de Véloci Ruben. "
 
         "Le raptor vous suit et essaie d'entrer, vous l'entendez parler. "
 
         "<< Rawr, je suis Véloci Ruben le Raptor, j'ai très faim, "
         "beaucoup trop faim. "
-
         "But i'm going to wait before eating you. "
-
         "Rawr, je veux des côtes. >>"
 
         "Et oui, dans ce jeu tout peut être bilingue."
